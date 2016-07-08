ac6af7f4cf94ff5c83bc02e5af5773652f3bacac
Paulina Lei <paulina.y.lei@gmail.com>
2016-07-07 23:20:24 -0700

    made a function to open modals B)

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
 var modal = document.getElementById('pic-modal');
 
 // Get the image and insert it inside the modal; use its "alt" text as a caption
-var img = document.getElementById('image');
+var img = document.getElementById('image2');
 var modalImage = document.getElementById("img01");
 var captionText = document.getElementById("caption");
 img.onclick = function(){
@@ -18,4 +18,16 @@ var closeImage = document.getElementsByClassName("close");
 // When the user clicks outside the image, close the modal
 closeImage.onclick = function() {
   modal.style.display = "none";
+}
+
+function openModal() {
+  var hamster = document.getElementById('image');
+  var modalImage2 = document.getElementById("img02");
+  var caption2 = document.getElementById("caption");
+  hamster.addEventListener('click', function() {
+    modal.style.display = "block";
+    modalImage2.src = this.src;
+    modalImage2.src = this.alt;
+    caption2.innerHTML = this.alt;
+  })
 }
