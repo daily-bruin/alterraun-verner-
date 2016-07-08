f347b57939aeb95800bc2adf64d17db0799d202f
Paulina Lei <paulina.y.lei@gmail.com>
2016-07-08 00:04:04 -0700

    made multiple modals work; using magic.css

--- js/app.js
+++ js/app.js
@@ -1,16 +1,5 @@
 // Get the modal
-var modal = document.getElementById('pic-modal');
-
-// Get the image and insert it inside the modal; use its "alt" text as a caption
-var img = document.getElementById('image2');
-var modalImage = document.getElementById("img01");
-var captionText = document.getElementById("caption");
-img.onclick = function(){
-    modal.style.display = "block";
-    modalImage.src = this.src;
-    modalImage.alt = this.alt;
-    captionText.innerHTML = this.alt;
-}
+var modal = document.getElementById("pic-modal");
 
 // Get the div element that closes the modal
 var closeImage = document.getElementsByClassName("close");
@@ -20,14 +9,26 @@ closeImage.onclick = function() {
   modal.style.display = "none";
 }
 
-function openModal() {
-  var hamster = document.getElementById('image');
-  var modalImage2 = document.getElementById("img02");
-  var caption2 = document.getElementById("caption");
-  hamster.addEventListener('click', function() {
-    modal.style.display = "block";
-    modalImage2.src = this.src;
-    modalImage2.src = this.alt;
-    caption2.innerHTML = this.alt;
-  })
+// Array of images to be placed in modal
+var imageArray = ["image1", "image2"];
+function openModal(index) {
+  var img = document.getElementById(imageArray[index]);
+  var modalImage = document.getElementById("modal-placement");
+  var captionText = document.getElementById("caption");
+  img.onclick = function(){
+      modal.style.display = "block";
+      modalImage.src = this.src;
+      modalImage.alt = this.alt;
+      captionText.innerHTML = this.alt;
+  }
 }
+
+// Opening all the modals
+for (var i = 0; i < imageArray.length; i++) {
+  openModal(i);
+}
+
+// jQuery and magic.css
+setTimeout(function() {
+  $(".headline").addClass('magictime puffIn ')
+})
